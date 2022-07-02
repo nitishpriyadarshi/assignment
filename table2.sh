c54082e67342ba7351624309301a51a9b76dc601
nitishpriyadarshi <nitish8152@gmail.com>
2022-07-02 18:38:18 +0530

    added

---
+++ table2.sh
@@ -0,0 +1,5 @@
+for (( cnt=2; cnt<=20; cnt=$((cnt+2))))
+do
+	echo $cnt;
+done
+
---
+++ tablepower.sh
@@ -0,0 +1,9 @@
+echo "Enter base value : "
+read a
+echo "Enter Power value : "
+read b
+while [ $a -gt 0 ]
+do
+  exp=$((m ^ n))
+echo $exp
+done
